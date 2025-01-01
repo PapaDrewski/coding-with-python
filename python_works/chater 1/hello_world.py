ff2cd949b0bdd8102e199768f2cd4205a2fd0fd0
PapaDrewski <anticsantyqs@gmail.com>
2025-01-01 01:17:02 -0500

    Add files via upload

---
+++ python_works/chater 1/hello_world.py
@@ -0,0 +1,49 @@
+print("Hello python world") #comment this out if you want
+                                                                            # how print works print = to output = to display or         
+                                                                            # inizlize the the programe this if for the mian function
+
+message = "Hello python world!"                                             # this is a variable every variable has a name and a value  
+print(message)                                                               # this is a function that will print the value of the variable message in the terminal
+
+
+
+
+message = "Hello python Crash Course world!"
+print(message)
+
+
+'''open
+When you are naming variables there are some rules you have to follow
+Variable names can contain only letters numbers and underscores They can start with a letter or an_but not with a number 
+for instance you can call variable message_1 but not one_message
+
+Spaces are not allowed in variable names but underscores can be used to separate words in a variable name
+for example greeting_message works but greeting message will cause errors
+
+
+Avoid using python keywords and function names as variable names
+for example you can't use the word print as a variable name because python uses it to display output
+
+Variable names should be short but descriptive
+for example name is better than n but student_name is even better
+
+Be careful when using the lowercase letter l and the uppercase letter O because they could be confused with the numbers 1 and 0
+
+Python is case sensitive which means that variables with different capitalization are different variables
+exsample is
+
+ message = "Hello python world!"                                             
+print(message)   
+
+Message = "Hello python Crash Course world!"
+print(Message)
+
+this will print out two different messages 
+Because one of the Ms. are capitalized thuss separate variables
+
+
+
+Python variables should be in lower case
+You don't get any error if they are in upper cased but it is a good practice to use lower case letters
+
+'''
---
+++ python_works/my project/mthas.py
@@ -0,0 +1,5 @@
+x = 8
+b = 9 
+
+taks_1 = (f"{x} + {b}")
+print(taks_1)
